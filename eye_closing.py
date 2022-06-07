175c80ab180918e68591e1c99fc394c08b74f207
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-06-08 04:36:12 +0530

    time based sleep and presence detection

--- eye_closing.py
+++ eye_closing.py
@@ -1,6 +1,5 @@
-import argparse
+import re
 import time
-
 import cv2
 import dlib
 import imutils
@@ -9,7 +8,6 @@ from imutils import face_utils
 from imutils.video import FileVideoStream, VideoStream
 from scipy.spatial import distance as dist
 
-
 def eye_aspect_ratio(eye):
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
@@ -17,42 +15,29 @@ def eye_aspect_ratio(eye):
     ear = (A + B) / (2.0 * C)
     return ear
 
-
-ap = argparse.ArgumentParser()
-ap.add_argument(
-    "-p", "--shape-predictor", required=True, help="path to facial landmark predictor"
-)
-ap.add_argument("-v", "--video", type=str, default="", help="path to input video file")
-args = vars(ap.parse_args())
-
-
 EYE_AR_THRESH = 0.3
 EYE_AR_CONSEC_FRAMES = 3
 
+CLOSED_TIME_THRESH = 3
+ABSENT_TIME_THRESH = 3
 
-COUNTER = 0
-TOTAL = 0
+closedTime = 0
+absentTime = 0
+text = 'NOT DETECTED'
 
-
-print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
-
+predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-
-print("[INFO] starting video stream thread...")
-vs = FileVideoStream(args["video"]).start()
+vs = FileVideoStream('').start()
 fileStream = True
 vs = VideoStream(src=0).start()
 fileStream = False
 time.sleep(1.0)
 
-
 while True:
-
     if fileStream and not vs.more():
         break
 
@@ -62,8 +47,17 @@ while True:
 
     rects = detector(gray, 0)
 
-    for rect in rects:
+    if not rects:
+        text = 'NOT PRESENT'
+        if absentTime == 0:
+            absentTime = time.time()
+        elif time.time() - absentTime >= ABSENT_TIME_THRESH:
+            text = 'ABSENT'
+    else:
+        absentTime = 0
+        text = 'PRESENT'
 
+    for rect in rects:
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
@@ -80,40 +74,24 @@ while True:
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
         if ear < EYE_AR_THRESH:
-            COUNTER += 1
-
+            text = 'CLOSED'
+            if closedTime == 0:
+                closedTime = time.time()
+            elif time.time() - closedTime >= CLOSED_TIME_THRESH:
+                text = 'SLEEPING'
         else:
+            closedTime = 0
+            text = 'PRESENT'
 
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                TOTAL += 1
-
-            COUNTER = 0
-
-        cv2.putText(
-            frame,
-            "Blinks: {}".format(TOTAL),
-            (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.7,
-            (0, 0, 255),
-            2,
-        )
-        cv2.putText(
-            frame,
-            "EAR: {:.2f}".format(ear),
-            (300, 30),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.7,
-            (0, 0, 255),
-            2,
-        )
-
+        cv2.putText( frame, "EAR: {:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    
+    cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord("q"):
         break
-
 
 cv2.destroyAllWindows()
 vs.stop()
